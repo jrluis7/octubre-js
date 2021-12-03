88c00cd6b766c9b64ed77edd624593f8170e5e6b
jrluis7 <jrluis7@gmail.com>
2021-12-03 16:49:54 +0100

    Commit incial

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+// Produccion -> Entorno de produccion , Entorno real
+
+// Desarrollo, local -> Entorno de desarrollo
+
+// Control de versiones de Software : GIT , Mercurial
+
+// Volver a versiones antiguas del desarrollo
+
+// Volver a versiones ESTABLES del desarrollo
+
+// Trabajar en grupo sobre un proyecto
+
+// Trabajar sobre un proyecto desde distintos ordenadores
+
+// CONSOLA -> Hay que aplicar comandos
+// Cliente de Git -> Sourcetree , guarda en local el estado del proyecto
+
+// Tracked - Git está pendiente de los cambios del archivo
+// Staged -> Los archivos están preparados
+// Commit -> Es una foto del código en ese punto. Un punto de guardado( Podremos volver a él si fuera necesario )
